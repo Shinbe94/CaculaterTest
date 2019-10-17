d0c04c78de1850c019cdfd562388d7ef3b918c1b
nhungnguyen <nhungnguyen>
2019-10-17 21:49:33 +0700

    Commit source code

--- src/test/java/Common/BaseClass.java
+++ src/test/java/Common/BaseClass.java
@@ -5,27 +5,14 @@ import java.net.URL;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 
 public class BaseClass {
 	protected AppiumDriver<MobileElement> driver;
-	protected ExtentHtmlReporter htmlReporter;
-	protected ExtentReports extent;
-
-	@BeforeSuite
-	public void reportSetup() {
-		htmlReporter = new ExtentHtmlReporter("extent.html");
-		extent = new ExtentReports();
-		extent.attachReporter(htmlReporter); 
-	}
-
+	
 	@BeforeTest
 	public void setup() {
 		try {
@@ -34,7 +21,7 @@ public class BaseClass {
 			cap.setCapability("uid", "emulator-5554");
 			cap.setCapability("platformName", "Android");
 			cap.setCapability("platformVersion", "8.1.0");
-			cap.setCapability("appPackage", "com.android.calculator2");
+			cap.setCapability("appPackage", "com.google.android.calculator");
 			cap.setCapability("appActivity", "com.android.calculator2.Calculator");
 
 			URL url = new URL("http://127.0.0.1:4723/wd/hub");
@@ -48,6 +35,5 @@ public class BaseClass {
 	@AfterSuite
 	public void tearDown() {
 		driver.closeApp();
-		extent.flush();
 	}
 }
--- src/test/java/Test/Calculation.java
+++ src/test/java/Test/Calculation.java
@@ -3,52 +3,39 @@ package Test;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
-
 import Common.BaseClass;
 import io.appium.java_client.MobileElement;
 
 public class Calculation extends BaseClass {
 
-	// Test phép cộng: 2 + 3 = 5
+	//Test plus: 2 + 3 = 5
 	@Test
 	public void testOne() {
-		// tạo một toggle cho test case, liệt kê tất cả nhật ký ghi được dưới nó.  
-        ExtentTest testOne = extent.createTest("MyFirstTest", "Sample description");
-        
-		MobileElement two = driver.findElement(By.id("com.android.calculator2:id/digit_2"));
+		MobileElement two = driver.findElement(By.id("com.google.android.calculator:id/digit_2"));
 		two.click();
-		testOne.log(Status.PASS, "Click two number");
 		
-		MobileElement plus = driver.findElementByAccessibilityId("plus");
+		MobileElement plus = driver.findElement(By.id("com.google.android.calculator:id/op_add"));
 		plus.click();
-		testOne.log(Status.PASS, "Click plus button");
-		
-		MobileElement three = driver.findElement(By.id("com.android.calculator2:id/digit_3"));
+		MobileElement three = driver.findElement(By.id("com.google.android.calculator:id/digit_3"));
 		three.click();
-		testOne.log(Status.PASS, "Click three number");
-		
-		MobileElement equals = driver.findElement(By.id("com.android.calculator2:id/eq"));
+		MobileElement equals = driver.findElement(By.id("com.google.android.calculator:id/eq"));
 		equals.click();
-		testOne.log(Status.PASS, "Click equals button");
-		
-		MobileElement result = driver.findElement(By.id("com.android.calculator2:id/result"));
+		MobileElement result = driver.findElement(By.id("com.google.android.calculator:id/formula"));
 		System.out.println("Result testOne: " + result.getText());
-		testOne.log(Status.INFO, "Test one completed");
 	}
 
-	// Test phép nhân: 5 * 2 = 10
+	//Test multiply: 5 * 2 = 10
+	@Test
 	public void testTwo() {
-		MobileElement five = driver.findElement(By.id("com.android.calculator2:id/digit_5"));
+		MobileElement five = driver.findElement(By.id("com.google.android.calculator:id/digit_5"));
 		five.click();
-		MobileElement multiply = driver.findElementByAccessibilityId("multiply");
+		MobileElement multiply = driver.findElement(By.id("com.google.android.calculator:id/op_mul"));
 		multiply.click();
-		MobileElement two = driver.findElement(By.id("com.android.calculator2:id/digit_2"));
+		MobileElement two = driver.findElement(By.id("com.google.android.calculator:id/digit_2"));
 		two.click();
-		MobileElement equals = driver.findElement(By.id("com.android.calculator2:id/eq"));
+		MobileElement equals = driver.findElement(By.id("com.google.android.calculator:id/eq"));
 		equals.click();
-		MobileElement result = driver.findElement(By.id("com.android.calculator2:id/result"));
+		MobileElement result = driver.findElement(By.id("com.google.android.calculator:id/formula"));
 		System.out.println("Result testTwo: " + result.getText());
 	}
 }
